6713ff20fde6fa5a186a9578c98701f1df567a92
Matthew Daniel Murphy <matt@mattmurphy.ca>
2024-10-27 04:02:34 -0700

    fix Report interface inheritance

--- src/interfaces/Report.tsx
+++ src/interfaces/Report.tsx
@@ -1,22 +1,16 @@
-export interface Report {
+
+export interface HoursTableReport {
     id?: number;
     date: string;
     name: string;
-    weather: string;
     time_in: string;
     time_out: string;
-    details: string;
-    tasks: string;
     is_overtime: boolean;
     overtime_hours: number;
 }
 
-export interface HoursTableReport extends Report {
-    id?: number;
-    date: string;
-    name: string;
-    time_in: string;
-    time_out: string;
-    is_overtime: boolean;
-    overtime_hours: number;
+export interface Report extends HoursTableReport {
+    weather: string;
+    details: string;
+    tasks: string;
 }
